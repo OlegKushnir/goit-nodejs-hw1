42647930e5542a3fb3003304ac4d621178cbd411
OlegKushnir <kushnir.oleg87@gmail.com>
2022-12-16 22:52:23 +0200

    update
    add commander

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const argv = require("yargs").argv;
 const {listContacts, getContactById, addContact, removeContact} = require('./contacts');
-console.log("Hello Node.js");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+const argv = program.opts();
 
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
