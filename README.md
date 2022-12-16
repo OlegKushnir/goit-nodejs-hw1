5bd56526c4350b5198f3a4b8ba599bc0d55d1321
OlegKushnir <kushnir.oleg87@gmail.com>
2022-12-16 22:19:30 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# goit-nodejs-hw1
+CLI app CONTACTS
+
+List contacts:
+https://ibb.co/YZ9yJDw
+
+Get contact by id:
+https://ibb.co/YcTW3Wm
+
+Add contact:
+https://ibb.co/fYTyJDn
+
+Remove contact by id:
+https://ibb.co/CwfRdFT
---
+++ contacts.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+const path = require("path");
+const contactsPath = "./db/contacts.json";
+const contacts = path.basename(contactsPath);
+
+function listContacts() {
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.table(JSON.parse(data));
+  });
+}
+
+function getContactById(contactId) {
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const contacts = JSON.parse(data);
+    const found = contacts.find((element) => element.id === String(contactId));
+    console.table(found);
+  });
+}
+
+function removeContact(contactId) {
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const contacts = JSON.parse(data);
+    const newContacts = contacts.filter((c) => c.id !== String(contactId));
+    if (contacts.length === newContacts.length) {
+      console.log("Contact not found");
+      return;
+    }
+    fs.writeFile(contactsPath, JSON.stringify(newContacts), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("Contact id=", contactId, " removed!");
+    });
+  });
+}
+
+function addContact(name, email, phone) {
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const contacts = JSON.parse(data);
+    const id = String(Number(contacts[contacts.length - 1].id) + 1);
+    const contact = { id, name, email, phone };
+    const newContacts = [...contacts, contact];
+    fs.writeFile(contactsPath, JSON.stringify(newContacts), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("New contact added! ");
+    });
+  });
+}
+module.exports = { listContacts, getContactById, removeContact, addContact };
